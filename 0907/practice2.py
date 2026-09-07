3e414759e4820448537f83e54af12b928e020b5c
BaekHyoYoung <108615164+HyoYoung0829@users.noreply.github.com>
2026-09-07 12:39:58 +0900

    코랩 파일 실행 연습

--- 0907/practice2.py
+++ 0907/practice2.py
@@ -9,7 +9,7 @@ class Fruit(BaseModel):
     # 'red' , 'green'으로 제한.
     color: Literal["red", "green"]
     # "Annotated" 기본 타입에 추가적인 메타데이터나 검증 규칙을 붙이는 문법
-    # 실수 타입이어야 하고, "0"이상이어야 함.
+    # 실수 타입이어야 하고, "0"초과이어야 함.
     weight: Annotated[float, Gt(0)]
     bazam: dict[str, list[tuple[int, bool, float]]]
 
